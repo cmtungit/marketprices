86ed276a09aaa2e5ffa59f67d89c0e2864ff409a
Tom Tung <cmtung.it@gmail.com>
2024-08-13 14:55:17 +0800

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,157 @@
+import "./App.css";
+import * as React from "react";
+import {
+  ProductList,
+  Searchfield,
+  BasicCard,
+  fetchProductImage,
+} from "./Components";
+import { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+
+export interface ProductProps {
+  code: string;
+  brand: {
+    en: string;
+    "zh-Hant": string;
+    "zh-Hans": string;
+  };
+  name: {
+    en: string;
+    "zh-Hant": string;
+    "zh-Hans": string;
+  };
+  cat1Name?: {
+    en: string;
+    "zh-Hant": string;
+    "zh-Hans": string;
+  };
+  cat2Name?: {
+    en: string;
+    "zh-Hant": string;
+    "zh-Hans": string;
+  };
+  cat3Name?: {
+    en: string;
+    "zh-Hant": string;
+    "zh-Hans": string;
+  };
+  prices: {
+    supermarketCode: string;
+    price: string;
+  }[];
+  offers?: {
+    supermarketCode: string;
+    en: string;
+    "zh-Hant": string;
+    "zh-Hans": string;
+  }[];
+  imageUrl?: string;
+  isFavorite: boolean;
+  handleFavorite: (product: any) => void;
+}
+
+const App = () => {
+  const [userInput, setUserInput] = useState("");
+  const [products, setProducts] = useState<ProductProps[]>([]);
+  const [filteredProducts, setFilteredProducts] = useState<ProductProps[]>([]);
+  const [favoriteProducts, setFavoriteProducts] = useState<ProductProps[]>([]);
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setUserInput(event.currentTarget.value);
+  }
+
+  const handleSearch = async (
+    originalProducts: ProductProps[],
+    searchValue: string
+  ) => {
+    const filtered = originalProducts
+      .filter((item) => item.name["zh-Hant"].includes(searchValue))
+      .filter((product) => product.prices.length !== 0);
+
+    // Fetch the images for each filtered product
+    const productsWithImages = await Promise.all(
+      filtered.map(async (product) => {
+        const imageUrl = await fetchProductImage(product.name["zh-Hant"]);
+        return { ...product, imageUrl };
+      })
+    );
+    setFilteredProducts(productsWithImages);
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      e.preventDefault(); // Prevent the default form submission behavior
+      handleSearch(products, userInput);
+    }
+  };
+
+  const handleFavorite = (product: ProductProps) => {
+    // Add or remove the product from the favoriteProducts state
+    if (favoriteProducts.some((p) => p.code === product.code)) {
+      setFavoriteProducts(
+        favoriteProducts.filter((p) => p.code !== product.code)
+      );
+    } else {
+      setFavoriteProducts([...favoriteProducts, product]);
+    }
+  };
+
+  // logs for data
+  useEffect(() => {
+    console.log(userInput);
+    console.log(products);
+    console.log(filteredProducts);
+  }, [filteredProducts]);
+
+  useEffect(() => {
+    // Load favorite products from localStorage on initial render
+    const storedFavorites = localStorage.getItem("favoriteProducts");
+    if (storedFavorites) {
+      setFavoriteProducts(JSON.parse(storedFavorites));
+    }
+    console.log(favoriteProducts);
+  }, []);
+
+  useEffect(() => {
+    // Save favorite products to localStorage whenever it changes
+    localStorage.setItem("favoriteProducts", JSON.stringify(favoriteProducts));
+  }, [favoriteProducts]);
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <ProductList products={products} setProducts={setProducts} />
+        <Searchfield
+          value={userInput}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {filteredProducts.map((product) => (
+            <Grid xs={2} sm={4} md={4} key={product.code}>
+              <BasicCard
+                code={product.code}
+                brand={product.brand}
+                name={product.name}
+                prices={product.prices}
+                offers={product.offers}
+                imageUrl={product.imageUrl}
+                isFavorite={favoriteProducts.some(
+                  (p) => p.code === product.code
+                )}
+                handleFavorite={() => handleFavorite(product)}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    </>
+  );
+};
+export default App;
---
+++ src/Components.tsx
@@ -0,0 +1,143 @@
+import * as React from "react";
+import { useState, useEffect } from "react";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Checkbox from "@mui/material/Checkbox";
+import FavoriteBorder from "@mui/icons-material/FavoriteBorder";
+import Favorite from "@mui/icons-material/Favorite";
+import { ProductProps } from "./App";
+
+interface SearchfieldProps {
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown: (event: React.KeyboardEvent<HTMLInputElement>) => void;
+}
+async function fetchAPI(): Promise<any> {
+  const APIUrl =
+    "https://online-price-watch.consumer.org.hk/opw/opendata/pricewatch.json";
+  const response = await fetch(APIUrl);
+  const result = await response.json();
+  console.log(result);
+  return result;
+}
+
+export function ProductList({
+  products,
+  setProducts,
+}: // setFilteredProducts,
+{
+  products: any[];
+  setProducts: React.Dispatch<React.SetStateAction<any[]>>;
+  // setFilteredProducts: React.Dispatch<React.SetStateAction<any[]>>;
+}) {
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const data = await fetchAPI();
+        setProducts(data);
+      } catch (err) {
+        console.error(err);
+        setError(error);
+      }
+    }
+    fetchData();
+  }, []);
+  return <div id="products">User: {JSON.stringify(products)}</div>;
+}
+
+export const Searchfield: React.FC<SearchfieldProps> = ({
+  value,
+  onChange,
+  onKeyDown,
+}) => {
+  return (
+    <div>
+      <TextField
+        fullWidth
+        id="outlined-search"
+        label="搵價錢"
+        type="search"
+        margin="normal"
+        value={value}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+      />
+    </div>
+  );
+};
+
+export const BasicCard: React.FC<ProductProps> = ({
+  code,
+  brand,
+  name,
+  prices,
+  offers,
+  imageUrl,
+  isFavorite,
+  handleFavorite,
+}) => {
+  return (
+    <Card>
+      <CardMedia
+        component="img"
+        height="200"
+        image={imageUrl}
+        alt={name["zh-Hant"]}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {brand["zh-Hant"]}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {name["zh-Hant"]}
+        </Typography>
+        {prices.map((price, index) => (
+          <Typography key={index} variant="body2" color="text.secondary">
+            {price.supermarketCode}: {price.price}
+          </Typography>
+        ))}
+        {offers?.map((offer, index) => (
+          <Typography key={index} variant="body2" color="text.secondary">
+            {offer.supermarketCode}: {offer["zh-Hant"]}
+          </Typography>
+        ))}
+      </CardContent>
+      <Checkbox
+        icon={<FavoriteBorder />}
+        checkedIcon={<Favorite color="error" />}
+        checked={isFavorite}
+        onChange={() =>
+          handleFavorite({
+            code,
+            brand,
+            name,
+            prices,
+            offers,
+            imageUrl,
+            isFavorite,
+            handleFavorite,
+          })
+        }
+      />
+    </Card>
+  );
+};
+export const fetchProductImage = async (
+  productName: string
+): Promise<string> => {
+  const searchQuery = encodeURIComponent(productName);
+  // console.log(searchQuery);
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?q=${searchQuery}&cx=373ff5a0ef6534bc7&key=AIzaSyBXFaaHIeTJJ6UkBaoHGXta0nz0BBV9JVE&num=1&searchType=image`
+  );
+  const data = await response.json();
+  console.log(data);
+  return data.items[0].link;
+};
